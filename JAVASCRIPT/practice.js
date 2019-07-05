dffe3be673a381d72c90fd2e4e869d31787ff58c
Uchenna <okorocode@gmail.com>
2019-07-05 07:26:17 +0100

    built a calculator

---
+++ JAVASCRIPT/practice.js
@@ -0,0 +1,7 @@
+// Building a calculator
+alert('Welcome to a simple Calculator, Have fun!!')
+let firstNumber = prompt('enter first Number');
+let secondNumber = prompt('enter second Number');
+
+const sum = Number(firstNumber) + Number(secondNumber);
+alert('The sum is :' + sum)
