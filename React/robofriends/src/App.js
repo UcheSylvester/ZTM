3ea649f655e3b9b82866153bc40e2f5bac2ebdf4
UcheSylvester <okorocode@gmail.com>
2019-09-08 21:03:41 +0100

    functionality as component

--- React/robofriends/src/App.js
+++ React/robofriends/src/App.js
@@ -1,41 +1,53 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import CardList from './CardList'
-import SearchBox from './SearchBox'
-import './App.css';
-import { robots } from './robots'
-
+import CardList from "./CardList";
+import SearchBox from "./SearchBox";
+import "./App.css";
+import Scroll from "./Scroll";
 
 class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            robots: robots,
-            searchField: ''
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchField: ""
+    };
+  }
 
-    onSearchChange = (event) => {
-        // setting searchField to be the search value
-        this.setState({ searchField: event.target.value })
-    }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users }));
+  }
 
-    render() {
-        // filtering robots
-        const filteredRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
-        })
-        // console.log(filteredRobots)
+  onSearchChange = event => {
+    // setting searchField to be the search value
+    this.setState({ searchField: event.target.value });
+  };
 
-        return (
-            <div className='tc'>
-                <h1>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filteredRobots} />
-            </div>
-        )
+  render() {
+    // filtering robots
+    const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchField.toLowerCase());
+    });
+    // console.log(filteredRobots)
+
+    if (!this.state.robots || this.state.robots.length === 0) {
+      return <h2 className="error-message tc">loading...</h2>;
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
+        </div>
+      );
     }
+  }
 }
 
-
-export default App
+export default App;
