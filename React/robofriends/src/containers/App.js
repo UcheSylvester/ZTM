e58512817e05dd8a0feeeeb9af328df62b29cf12
UcheSylvester <okorocode@gmail.com>
2019-09-08 23:51:54 +0100

    using middleware

--- React/robofriends/src/containers/App.js
+++ React/robofriends/src/containers/App.js
@@ -7,45 +7,41 @@ import "./App.css";
 import Scroll from "../components/Scroll";
 import ErrorBoundary from "../components/ErrorBoundry";
 
-import { setSearchField } from '../action'
+import { setSearchField, requestRobots } from '../action'
+// import { requestRobots } from "../reducers";
 
 const mapStateToProps = state => {
+  console.log(state)
   return {
-    searchField: state.searchField
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => requestRobots(dispatch)
   }
 }
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: []
-    };
-  }
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     robots: []
+  //   };
+  // }
 
   componentDidMount() {
     // console.log(this.props.store.getState())
-
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(response => response.json())
-      .then(users => this.setState({ robots: users }));
+    this.props.onRequestRobots()
   }
 
-  // onSearchChange = event => {
-  //   // setting searchField to be the search value
-  //   this.setState({ searchField: event.target.value });
-  // };
-
   render() {
-
-    const { robots } = this.state;
-    const { searchField, onSearchChange } = this.props;
+    const { searchField, onSearchChange, robots, isPending, error } = this.props;
 
     // filtering robots
     const filteredRobots = robots.filter(robot => {
@@ -54,8 +50,8 @@ class App extends Component {
         .includes(searchField.toLowerCase());
     });
 
-    // using ternary to check for the when there is robots
-    return (!robots.length) ?
+    // using ternary to check for the when request is still pending
+    return isPending ?
       <h2 className="error-message tc">loading...</h2> :
       (
         <div className="tc">
@@ -67,7 +63,25 @@ class App extends Component {
             </ErrorBoundary>
           </Scroll>
         </div>
-      );
+      )
+
+    // if (isPending) {
+    //   return <h2 className="error-message tc">loading...</h2>
+    // } else if (!isPending && robots) {
+    //   return (
+    //     <div className="tc">
+    //       <h1 className="f1">RoboFriends</h1>
+    //       <SearchBox searchChange={onSearchChange} />
+    //       <Scroll>
+    //         <ErrorBoundary>
+    //           <CardList robots={filteredRobots} />
+    //         </ErrorBoundary>
+    //       </Scroll>
+    //     </div>
+    //   )
+    // } else {
+    //   return <h1>{error}</h1>
+    // }
 
   }
 }
--- React/robofriends/src/reducers.js
+++ React/robofriends/src/reducers.js
@@ -1,14 +1,40 @@
-import { CHANGE_SEARCH_FIELD } from "./constant";
+import {
+    CHANGE_SEARCH_FIELD,
+    REQUEST_ROBOT_PENDING,
+    REQUEST_ROBOT_SUCCESS,
+    REQUEST_ROBOT_FAILED
+} from "./constant";
 
-const intialState = {
+const intialStateSearch = {
     searchField: ''
 };
 
-export const searchRobots = (state = intialState, action = {}) => {
-    console.log(action.type)
+// reducer for searching robots
+export const searchRobots = (state = intialStateSearch, action = {}) => {
     switch (action.type) {
         case CHANGE_SEARCH_FIELD:
             return Object.assign({}, state, { searchField: action.payload });
+        default:
+            return state;
+    }
+}
+
+
+const intialStateRobots = {
+    isPending: true,
+    robots: [],
+    error: ''
+}
+
+// reducer for fetching robot
+export const requestRobots = (state = intialStateRobots, action = {}) => {
+    switch (action.type) {
+        case REQUEST_ROBOT_PENDING:
+            return Object.assign({}, state, { isPending: true });
+        case REQUEST_ROBOT_SUCCESS:
+            return Object.assign({}, state, { robots: action.payload, isPending: false })
+        case REQUEST_ROBOT_FAILED:
+            return Object.assign({}, state, { error: action.payload, isPending: false })
         default:
             return state;
     }
